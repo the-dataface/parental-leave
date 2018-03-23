12a30705ba16e5ab29612af35378c92086fa6499
Michael Hester <michaelvhester@gmail.com>
2018-03-23 08:59:58 -0400

    fixin for safari hopefully

--- js/d3-tip.js
+++ js/d3-tip.js
@@ -34,7 +34,7 @@
     var direction = d3TipDirection,
       offset = d3TipOffset,
       html = d3TipHTML,
-      rootElement = document.getElementById("pL-full-header-container"),
+      rootElement = document.body,
       node = initNode(),
       svg = null,
       point = null,
